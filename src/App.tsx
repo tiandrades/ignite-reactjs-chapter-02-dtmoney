604c50a71aef22327a3a1bb028d11e07f260a46e
Tiago Andrade <tailergamer7@gmail.com>
2021-03-19 16:42:46 -0400

    :sparkles: feat(lesson: Carregando transações): listing transactions

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 
 import { NewTransactionModal } from './components/NewTransactionModal';
-import { TransactionsContext } from './TransactionsContext';
+import { TransactionsProvider } from './TransactionsContext';
 
 import { GlobalStyle } from './styles/global';
 
@@ -23,7 +23,7 @@ export function App() {
   }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -34,6 +34,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
